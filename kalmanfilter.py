3f29913a53cbbedb5dfd1860947939f8b6d92f29
Lion1Blue <lionhillebrand@gmx.de>
2022-05-25 10:08:06 +0200

    Added Comments to existing code

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -7,6 +7,7 @@ class KalmanFilter:
     kf = cv2.KalmanFilter(4, 2)
     kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
     kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+    
 
 
     def predict(self, coordX, coordY):
@@ -18,5 +19,10 @@ class KalmanFilter:
         x, y = int(predicted[0]), int(predicted[1])
         
         return x, y
+
+    def predictWithoutCorrect(self):
+        predicted = self.kf.predict()
+
+        return int(predicted[0]), int(predicted[1])
     
 
